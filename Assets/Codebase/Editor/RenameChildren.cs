97e0e3ad380101231a52e4d2d46fdfd34a3bdca3
Ivan <ionufriev95@gmail.com>
2024-01-23 19:32:17 +0600

    Red desert map

--- Assets/Codebase/Editor/RenameChildren.cs
+++ Assets/Codebase/Editor/RenameChildren.cs
@@ -1,25 +1,29 @@
-
-using System;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-public class RenameChildren : EditorWindow {
+public class RenameChildren : EditorWindow
+{
     private static readonly Vector2Int size = new Vector2Int(250, 100);
     private string childrenPrefix;
     private int startIndex;
     private List<string> names;
-    [MenuItem("GameObject/Rename children")] public static void ShowWindow() {
+    [MenuItem("GameObject/Rename children")]
+    public static void ShowWindow()
+    {
         EditorWindow window = GetWindow<RenameChildren>();
         window.minSize = size;
         window.maxSize = size;
     }
-    private void OnGUI() {
+    private void OnGUI()
+    {
         childrenPrefix = EditorGUILayout.TextField("Children prefix", childrenPrefix);
         startIndex = EditorGUILayout.IntField("Start index", startIndex);
-        if (GUILayout.Button("Rename children")) {
+        if (GUILayout.Button("Rename children"))
+        {
             GameObject[] selectedObjects = Selection.gameObjects;
-            for (int objectI = 0; objectI < selectedObjects.Length; objectI++) {
+            for (int objectI = 0; objectI < selectedObjects.Length; objectI++)
+            {
                 Transform selectedObjectT = selectedObjects[objectI].transform;
                 for (int childI = 0, i = startIndex; childI < selectedObjectT.childCount; childI++)
                 {
