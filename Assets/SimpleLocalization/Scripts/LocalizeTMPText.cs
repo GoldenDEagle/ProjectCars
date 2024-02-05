26266e778323c5a440a33f8d66b1e30d333c8e8f
Ivan <ionufriev95@gmail.com>
2024-02-05 15:16:08 +0600

    Car selection localizaed + some ingame UI

---
+++ Assets/SimpleLocalization/Scripts/LocalizeTMPText.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.SimpleLocalization.Scripts
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class LocalizeTMPText : MonoBehaviour
+    {
+        public string LocalizationKey;
+
+        public void Start()
+        {
+            Localize();
+            LocalizationManager.LocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.LocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            GetComponent<TMP_Text>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+    }
+}
