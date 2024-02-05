1b48e7469cd62174ec3967a44213a5cabded062f
Ivan <ionufriev95@gmail.com>
2024-02-05 17:09:37 +0600

    Endgame window

--- Assets/Codebase/Presenters/Endgame/EndgamePresenter.cs
+++ Assets/Codebase/Presenters/Endgame/EndgamePresenter.cs
@@ -1,5 +1,6 @@
 using Assets.Codebase.Infrastructure.ServicesManagment;
 using Assets.Codebase.Infrastructure.ServicesManagment.Ads;
+using Assets.Codebase.Infrastructure.ServicesManagment.Localization;
 using Assets.Codebase.Presenters.Base;
 using Assets.Codebase.Presenters.Endgame;
 using Assets.Codebase.Utils.Values;
@@ -12,6 +13,14 @@ public class EndgamePresenter : BasePresenter, IEndgamePresenter
     public ReactiveProperty<bool> DoubleRewardButtonActiveState { get; private set; }
     public ReactiveProperty<string> PositionString { get; private set; }
     public ReactiveProperty<string> CoinRewardString { get; private set; }
+
+    private const string FirstPlaceKey = "placement_first";
+    private const string SecondPlaceKey = "placement_second";
+    private const string ThirdPlaceKey = "placement_third";
+    private const string FourthPlaceKey = "placement_fourth";
+    private const string FifthPlaceKey = "placement_fifth";
+    private const string SixthPlaceKey = "placement_sixth";
+    private const string DefaultPlacementKey = "placement_default";
 
     private IDisposable _rewardedSubscription;
 
@@ -26,7 +35,7 @@ public class EndgamePresenter : BasePresenter, IEndgamePresenter
     protected override void SubscribeToModelChanges()
     {
         base.SubscribeToModelChanges();
-        GameplayModel.CurrentReward.Subscribe(value => CoinRewardString.Value = "Reward: " + value).AddTo(CompositeDisposable);
+        GameplayModel.CurrentReward.Subscribe(value => CoinRewardString.Value = value.ToString()).AddTo(CompositeDisposable);
     }
 
     public override void CreateView()
@@ -34,7 +43,7 @@ public class EndgamePresenter : BasePresenter, IEndgamePresenter
         base.CreateView();
         //CoinRewardString.Value = "Reward: " + GameplayModel.CurrentReward.Value;
         DoubleRewardButtonActiveState.Value = ServiceLocator.Container.Single<IAdsService>().CheckIfRewardedIsAvailable();
-        PositionString.Value = "Position: " + GameplayModel.ActiveRace.Value.Result.Position.ToString();
+        PositionString.Value = CreatePlacementString(GameplayModel.ActiveRace.Value.Result.Position);
     }
 
     public void ContinueButtonClicked()
@@ -59,5 +68,38 @@ public class EndgamePresenter : BasePresenter, IEndgamePresenter
     {
         CompositeDisposable.Remove(_rewardedSubscription);
         GameplayModel.IncreaseReward();
+    }
+
+    private string CreatePlacementString(int position)
+    {
+        var localizationService = ServiceLocator.Container.Single<ILocalizationService>();
+        string placementString = string.Empty;
+
+        switch (position)
+        {
+            case 1:
+                placementString = localizationService.LocalizeTextByKey(FirstPlaceKey);
+                break;
+            case 2:
+                placementString = localizationService.LocalizeTextByKey(SecondPlaceKey);
+                break;
+            case 3:
+                placementString = localizationService.LocalizeTextByKey(ThirdPlaceKey);
+                break;
+            case 4:
+                placementString = localizationService.LocalizeTextByKey(FourthPlaceKey);
+                break;
+            case 5:
+                placementString = localizationService.LocalizeTextByKey(FifthPlaceKey);
+                break;
+            case 6:
+                placementString = localizationService.LocalizeTextByKey(SixthPlaceKey);
+                break;
+            default:
+                placementString = localizationService.LocalizeTextByKey(DefaultPlacementKey);
+                break;
+        }
+
+        return placementString;
     }
 }
--- Assets/Codebase/Presenters/Ingame/IngamePresenter.cs
+++ Assets/Codebase/Presenters/Ingame/IngamePresenter.cs
@@ -1,12 +1,16 @@
 using Assets.Codebase.Presenters.Base;
 using Assets.Codebase.Views.Base;
+using Assets.SimpleLocalization.Scripts;
 using System;
 using UniRx;
+using UnityEditor.Localization.Editor;
 
 public class IngamePresenter : BasePresenter, IIngamePresenter
 {
     public ReactiveProperty<string> PositionString { get; private set; }
     public ReactiveProperty<string> LapString { get; private set; }
+
+    private const string ValueSeparator = " / ";
 
     private int _numberOfEnemies = 0;
     private int _numberOfLaps = 1;
@@ -36,11 +40,11 @@ public class IngamePresenter : BasePresenter, IIngamePresenter
 
     private void CalculatePosition(int currentPosition)
     {
-        PositionString.Value = currentPosition + " / " + (_numberOfEnemies + 1);
+        PositionString.Value = currentPosition + ValueSeparator + (_numberOfEnemies + 1);
     }
 
     private void CalculateLaps(int currentLap)
     {
-        LapString.Value = currentLap + " / " + (_numberOfLaps);
+        LapString.Value = currentLap + ValueSeparator + (_numberOfLaps);
     }
 }
